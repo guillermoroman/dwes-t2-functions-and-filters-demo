583bb95666b500f066a7f17aec8063eb004a4db2
guillermoroman <guillermoroman@outlook.com>
2024-10-17 11:40:30 +0200

    first commit

--- index.php
+++ index.php
@@ -6,6 +6,36 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
+?>
+
+<ul>
+    <?php foreach ($books as $book) : ?>
+        <li>
+            <?= $book['name'] ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
 </body>
 </html>
